d1446cdbeca1403c1d6672460c5eb9910f610cd8
seung-heee <tmdgmldi2595@naver.com>
2023-10-25 23:43:42 +0900

    accordion section

--- src/components/Home/Accordion.js
+++ src/components/Home/Accordion.js
@@ -6,17 +6,29 @@ const Accordion = ({ level, selected, setSelected, showText, setShowText }) => {
         setShowText(true);
     };
 
+    const Title = [
+        <h3>"가상 동반자를 현실로” :<br /> 유기견 입양 캠페인1</h3>,
+        <h3>"가상 동반자를 현실로” :<br /> 유기견 입양 캠페인2</h3>,
+        <h3>"가상 동반자를 현실로” :<br /> 유기견 입양 캠페인3</h3>,
+        <h3>"가상 동반자를 현실로” :<br /> 유기견 입양 캠페인4</h3>]
+
+    const Sub = [
+        "PIU는 실제 유기되어 안락사를 기다리는 반려견들로 패션쇼를 진행합니다.",
+        "PIU는 실제 유기되어 안락사를 기다리는 반려견들로 패션쇼를 진행합니다.",
+        "PIU는 실제 유기되어 안락사를 기다리는 반려견들로 패션쇼를 진행합니다.",
+        "PIU는 실제 유기되어 안락사를 기다리는 반려견들로 패션쇼를 진행합니다."
+    ]
+
     return (
         <div className={`slideItem slideItem${level} ${selected === level ? "active grow" : ""}`} onClick={() => { handleActive(level) }}>
             {showText &&
                 <>
                     <div>
                         <p className='AccordionTitle pretendard-EB text-focus-in'>
-                            “가상 동반자를 현실로”<br /> : 유기견 입양 캠페인
+                            {Title[level]}
                         </p>
-                        <p className='AccordionSub pretendard-R text-focus-in'>
-                            부재 캠페인 목적부재 캠페인 목적부재 캠페인 목적부재 캠페인 목적부재 캠페인 목적부재 캠페인 목적부재 캠페인 목적부재 캠페인 목적부
-                            재 캠페인 목적부재
+                        <p className='AccordionSub text-md pretendard-B text-focus-in'>
+                            {Sub[level]}
                         </p>
                     </div>
                 </>}
