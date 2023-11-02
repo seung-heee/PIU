e4c59974e44247a0827da18d2a42e10d09698ee9
minseo121 <kittse1217@gmail.com>
2023-11-02 20:39:28 +0900

    슬라이드 1차 구현

--- src/components/Home/Purchase2.js
+++ src/components/Home/Purchase2.js
@@ -1,9 +1,102 @@
+import React from 'react';
+import '../../css/Purchase.css';
+import { images } from '../../utils/images';
+
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Pagination, EffectCoverflow, Autoplay } from 'swiper'
+import '../../../node_modules/swiper/swiper-bundle.esm.js'
+import '../../../node_modules/swiper/swiper.min.css'
+
+const slider = [
+    {
+        url: "https://images.unsplash.com/photo-1612240498936-65f5101365d2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+    },
+    {
+        url: "https://images.unsplash.com/photo-1551024601-bec78aea704b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80"
+    },
+    {
+        url: "https://images.unsplash.com/photo-1612240498936-65f5101365d2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+    },
+    {
+        url: "https://images.unsplash.com/photo-1551024601-bec78aea704b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80"
+    }
+]
 const Purchase2 = () => {
-    return (
-        <div>
-            민서2
+
+
+  return (
+    <div className="section min-h-screen">
+        <div className='carousel flex justify-center items-center'>
+            <Swiper
+            className='myswiper'
+            modules={[Pagination, EffectCoverflow, Autoplay]}
+            effect={"coverflow"}
+            grabCursor={true}
+            centeredSlides={true}
+            coverflowEffect={{
+                rotate: 0,
+                stretch: 0,
+                depth: 100,
+                modifier: 3,
+                slideShadows: true
+            }}
+            loop={true}
+            pagination={{clickable: true}}
+
+            autoplay={{
+                delay: 5000,
+                disableOnInteraction: false
+            }}
+            breakpoints={{
+                640: {
+                    slidesPerView: 2
+                },
+                768: {
+                    slidesPerView: 1
+                },
+                1024: {
+                    slidesPerView: 2
+                },
+                1560: {
+                    slidesPerView: 3
+                },
+            }}
+            
+            >
+                {
+                    slider.map(data=> (
+                        <SwiperSlide style={{ backgroundImage: `url(${data.url})` }} className="myswiper-slider">
+                            <div>
+                                <br/>
+                                <br/>
+                                <br/>
+                                <br/>
+                                <br/>
+                                <br/>
+                                <br/>
+                                <br/>
+                                <br/>
+                                <br/>
+                                <br/>
+                                <br/>
+                                <br/>
+                                <br/>
+                                <br/>
+                                <br/>
+                                <br/>
+                                <br/>
+                                <br/>
+                                <br/>
+                                <br/>
+                                <br/>
+                            </div>
+                        </SwiperSlide>
+                    ))
+                }
+            </Swiper>
         </div>
-    )
-}
+    </div>
+  );
+};
 
 export default Purchase2;
