08c7c658d935184c1b07a1eb5e1bea8b464dc217
minseo121 <kittse1217@gmail.com>
2023-11-06 03:09:44 +0900

    슬라이드 최선 구현

--- src/components/Home/Purchase2.js
+++ src/components/Home/Purchase2.js
@@ -3,98 +3,88 @@ import '../../css/Purchase.css';
 import { images } from '../../utils/images';
 
 import { Swiper, SwiperSlide } from 'swiper/react'
-import { Pagination, EffectCoverflow, Autoplay } from 'swiper'
-import '../../../node_modules/swiper/swiper-bundle.esm.js'
-import '../../../node_modules/swiper/swiper.min.css'
+import { Pagination, EffectCoverflow, Navigation } from 'swiper'
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css";
 
-const slider = [
-    {
-        url: "https://images.unsplash.com/photo-1612240498936-65f5101365d2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
-    },
-    {
-        url: "https://images.unsplash.com/photo-1551024601-bec78aea704b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80"
-    },
-    {
-        url: "https://images.unsplash.com/photo-1612240498936-65f5101365d2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
-    },
-    {
-        url: "https://images.unsplash.com/photo-1551024601-bec78aea704b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80"
-    }
-]
+/*const imageUrls = [
+    { src: images.tok },
+    { src: images.tok },
+    { src: images.tok },
+    { src: images.tok },
+    { src: images.tok }
+  ];*/
+  
 const Purchase2 = () => {
-
-
   return (
     <div className="section min-h-screen">
-        <div className='carousel flex justify-center items-center'>
+        <div className='carousel flex justify-center'>
             <Swiper
             className='myswiper'
-            modules={[Pagination, EffectCoverflow, Autoplay]}
+            modules={[Pagination, EffectCoverflow, Navigation]}
             effect={"coverflow"}
-            grabCursor={true}
-            centeredSlides={true}
+            centeredSlides={"true"}
             coverflowEffect={{
                 rotate: 0,
-                stretch: 0,
-                depth: 100,
-                modifier: 3,
+                stretch: 100,
+                depth: 50,
+                modifier: 1.5,
                 slideShadows: true
             }}
             loop={true}
-            pagination={{clickable: true}}
-
-            autoplay={{
-                delay: 5000,
-                disableOnInteraction: false
-            }}
+            loopAdditionalSlides={1}
+            pagination={{
+                el: '.swiper-pagination',
+                      clickable: true
+              }}
+            slidesPerView={2}
+            spaceBetween={10}
+            navigation={{
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+              }}
             breakpoints={{
-                640: {
-                    slidesPerView: 2
+                '@0.75':{
+                    slidesPerView:1,
+                    spaceBetween:50,
                 },
-                768: {
-                    slidesPerView: 1
+                '@1.00':{
+                    slidesPerView:2,
+                    spaceBetween:40,
                 },
-                1024: {
-                    slidesPerView: 2
-                },
-                1560: {
-                    slidesPerView: 3
-                },
-            }}
-            
-            >
-                {
-                    slider.map(data=> (
-                        <SwiperSlide style={{ backgroundImage: `url(${data.url})` }} className="myswiper-slider">
-                            <div>
-                                <br/>
-                                <br/>
-                                <br/>
-                                <br/>
-                                <br/>
-                                <br/>
-                                <br/>
-                                <br/>
-                                <br/>
-                                <br/>
-                                <br/>
-                                <br/>
-                                <br/>
-                                <br/>
-                                <br/>
-                                <br/>
-                                <br/>
-                                <br/>
-                                <br/>
-                                <br/>
-                                <br/>
-                                <br/>
-                            </div>
-                        </SwiperSlide>
-                    ))
+                '@1.50':{
+                    slidesPerView:2,
+                    spaceBetween: 50,
                 }
+            }}
+            >
+                <SwiperSlide>
+                    <img src={images.tok} alt="그립톡" className="giftimg" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={images.tok} alt="그립톡" className="giftimg" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={images.tok} alt="그립톡" className="giftimg" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={images.tok} alt="그립톡" className="giftimg" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={images.tok} alt="그립톡" className="giftimg" />
+                </SwiperSlide>
+
+                <div className="swiper-button-next"></div>
+                <div className="swiper-button-prev"></div>
+                <div className='swiper-pagination'></div>
             </Swiper>
         </div>
+        <div className='button1 flex justify-center items-center w-full'>
+                <button type='button' className="py-5 px-9 text-xl font-semibold rounded-lg shadow-md text-white bg-slate-800 hover:bg-white hover:text-slate-800">
+                        정기후원하고 굿즈받기
+                    </button>
+                </div>
     </div>
   );
 };
