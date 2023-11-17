d95fb27c33d5ab2f016f1a511132c0ae28d36518
minseo121 <kittse1217@gmail.com>
2023-11-17 11:41:23 +0900

    1차 화면 구성

--- src/components/buy/BuyBottom.js
+++ src/components/buy/BuyBottom.js
@@ -1,6 +1,47 @@
+import { images } from '../../utils/images';
+
 const BuyBottom = () => {
     return (
-        <div>민서야 여기~~</div>
+        <div className="BuyBottom mt-10">
+                <div className="container1 mx-auto">
+                        <div className="w-full flex min-h-screen max-h-screen bg-green-300">
+                            <div className="w-1/2 flex items-center justify-center ">
+                            <p className="text-7xl font-black text-white mb-16">[PIU]<br/>
+                                 <span className='mt-10'>그립톡 굿즈</span></p><br/>
+                            <p className='text-white'>"세상에 단 하나뿐인 반려견 그립톡"</p>
+                            </div>
+                            
+                            <div className="w-1/2 flex min-h-screen max-h-screen">
+                                <img src={images.buyb_img1} alt="패키지 이미지" className="pt-28 pl-28"/>
+                                </div>
+                        </div>
+                    </div>
+                    <div className="container2 mx-auto">
+                        <div className="w-full flex min-h-screen max-h-screen bg-rose-100">
+                            <div className="w-1/2 flex items-center justify-center ">
+                                <p className="text-7xl font-black mb-16">[PIU]<br/>
+                                    <span className='mt-10'>그립톡 굿즈</span></p><br/>
+                                <p className=''>"세상에 단 하나뿐인 반려견 그립톡"</p>
+                                </div>
+                                
+                            <div className="w-1/2 flex min-h-screen max-h-screen">
+                                <img src={images.buyb_img2} alt="패키지 이미지" className="pt-28 px-14"/>
+                                </div>
+                        </div>
+                    </div>
+                    <div className="container3 mx-auto">
+                        <div className="w-full flex min-h-screen max-h-screen bg-pink-100">
+                        <div className="w-1/2 flex min-h-screen max-h-screen">
+                                    <img src={images.buyb_img3} alt="패키지 이미지" className="pt-28 px-14"/>
+                                    </div>
+                            <div className="w-1/2 flex items-center justify-center ">
+                                    <p className="text-7xl font-black mb-16">[PIU]<br/>
+                                        <span className='mt-10'>그립톡 굿즈</span></p><br/>
+                                    <p className=''>"세상에 단 하나뿐인 반려견 그립톡"</p>
+                                    </div>
+                        </div>
+                    </div>
+                </div>
     )
 }
 
--- src/pages/Buy.js
+++ src/pages/Buy.js
@@ -10,24 +10,47 @@ const Buy = () => {
             <div className="Buy">
                 <div className="container mx-auto">
                     <div className="w-full flex">
-                        <div className="w-1/2"><img src={images.buy1} alt="패키지 이미지" className="p-8" /></div>
-                        <div className="w-1/2">
-                            <p>☆코스튬 기획전☆[도그웨그] 공작새
-                                강아지 코스튬 고양이 할로윈 코스프레 옷 [오프라인전용]</p>
+                        <div className="w-1/2"><img src={images.buy1} alt="패키지 이미지" className="p-8 ml-8" /></div>
+                        <div className="w-1/2 p-16 pt-24 pr-24">
+                            <p className="text-xl mb-5 font-medium">☆코스튬 기획전☆ [도그웨그] 공작새<br/>
+                                강아지 코스튬 고양이 할로윈 코스프레 옷 <br/> [오프라인전용]</p>
                             <div>
-                                <form className="flex flex-col">
-                                    <select name="Brand">
+                                <form className="flex flex-col mr-3 pr-5">
+                                    <select name="Brand" className="h-12 my-4 p-3 outline outline-1 rounded-md">
                                         <option value="브랜드A" selected="selected">브랜드A</option>
                                         <option value="브랜드B">브랜드B</option>
                                         <option value="브랜드C">브랜드C</option>
                                     </select>
-                                    <select name="Size">
+                                    <select name="Size" className="h-12 mb-6 p-3 outline outline-1 rounded-md">
                                         <option value="S">S</option>
                                         <option value="M">M</option>
                                         <option value="L">L</option>
                                     </select>
                                     <output name="result" for="a b"></output>
-                                    <input type="submit" value="구매하기" />
+                                    <div className="mb-6">
+                                        <p className="w-1/2 text-xl px-2 font-medium">수량</p>
+                                    </div>
+                                    <div className="mb-6">
+                                        <p className="w-1/2 text-2xl px-2 font-semibold">총 상품 금액</p>
+                                    </div>
+                                    <hr/>
+                                    <div className="w-full flex">
+                                    <input type="submit" value="구매하기" className="my-5 ml-5 p-4 bg-black w-3/5 text-white rounded-xl text-xl font-semibold" />
+                                    <button url="" className="ml-8">
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-10 h-10">
+                                        <path fillRule="evenodd" d="M5.337 21.718a6.707 6.707 0 01-.533-.074.75.75 0 01-.44-1.223 3.73 3.73 0 00.814-1.686c.023-.115-.022-.317-.254-.543C3.274 16.587 2.25 14.41 2.25 12c0-5.03 4.428-9 9.75-9s9.75 3.97 9.75 9c0 5.03-4.428 9-9.75 9-.833 0-1.643-.097-2.417-.279a6.721 6.721 0 01-4.246.997z" clipRule="evenodd" />
+                                        </svg>
+                                    </button>
+                                    <button url="" className="ml-4">
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-10 h-10">
+                                        <path fillRule="evenodd" d="M15.75 4.5a3 3 0 11.825 2.066l-8.421 4.679a3.002 3.002 0 010 1.51l8.421 4.679a3 3 0 11-.729 1.31l-8.421-4.678a3 3 0 110-4.132l8.421-4.679a3 3 0 01-.096-.755z" clipRule="evenodd" />
+                                        </svg>
+                                    </button>
+                                    </div>
+                                    <div className="px-10">
+                                        <p className="text-right">누적 판매 1000+
+                                        </p>
+                                    </div>
                                 </form>
                             </div>
                         </div>
