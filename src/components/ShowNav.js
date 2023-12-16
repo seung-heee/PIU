ec2c558ef6021c7676fb3651d1a9ee2a3071d7bd
seung-heee <tmdgmldi2595@naver.com>
2023-12-16 19:57:46 +0900

    fix nav

--- src/components/ShowNav.js
+++ src/components/ShowNav.js
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import { images } from "../utils/images";
 
 const ShowNav = () => {
     return (
@@ -6,9 +7,10 @@ const ShowNav = () => {
             <ul className="min-h-screen flex flex-col items-center justify-center gap-y-6 text-3xl">
                 <li className="hover:textShadow duration-500 hover:translate-y-[-5px]"><Link to="#">패키지 구매</Link></li>
                 <li className="hover:textShadow duration-500 hover:translate-y-[-5px]"><Link to="#">주문 | 배송 조회</Link></li>
-                <li className="hover:textShadow duration-500 hover:translate-y-[-5px]"><Link to="#">후원하기</Link></li>
-                <li className="hover:textShadow duration-500 hover:translate-y-[-5px] RunningPIU"><Link to="#">Running PIU App</Link></li>
-                <li className="mb-20 hover:textShadow duration-500 hover:translate-y-[-5px]"></li>
+                <li className="hover:textShadow duration-500 hover:translate-y-[-5px]"><Link to="https://www.spatial.io/s/PIU-654bc9ef10ee2f1f26714032?share=3274268876320783038">
+                    <img src={images.RunningPIUApp} alt="" className='w-60' /></Link></li>
+                {/* <li className="hover:textShadow duration-500 hover:translate-y-[-5px] RunningPIU"><Link to="#">Running PIU App</Link></li> */}
+                {/* <li className="mb-20 hover:textShadow duration-500 hover:translate-y-[-5px]"></li> */}
             </ul>
         </div>
     )
